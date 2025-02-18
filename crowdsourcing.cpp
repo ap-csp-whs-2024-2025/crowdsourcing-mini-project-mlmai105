1f850acf50ee8537cedb2d88625a945815c3dd29
mlmai105 <mollyle43@gmail.com>
2025-02-18 13:50:54 -0800

    Update crowdsourcing.cpp

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -11,8 +11,30 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+std::vector<std::string> (singleIndex or stringsong, intduration, stringartist)
+{
+    while (count < size)
+    {
+        if 
+    }
+}
+
+
+
 int main()
 {
     // write your code here
+
+    // std::vector songList = 
+
+
+
+    std::cout << "\nEnter the song's title: "; 
+
+    std::cout << "\nEnter artist name: ";
+
+    std::cout << "\nEneter duration of the song: "
+
+    std::vector<std::string> user_song = {};
     return 0;
 }
