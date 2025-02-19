41ea579a293a0a5c34588eaea6b59a240c65ca4d
mlmai105 <mollyle43@gmail.com>
2025-02-19 21:06:57 +0000

    progress

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -11,30 +11,48 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
-std::vector<std::string> (singleIndex or stringsong, intduration, stringartist)
-{
-    while (count < size)
-    {
-        if 
-    }
-}
+// std::vector<std::string> (std::vector<std::string> user_song)
+// {
+//     while (count < size)
+//     {
+//         if 
+//         {
+//             append(user_song, )
+//         }
+//     }
+
+//     return user_song
+// }
+
+
 
 
 
 int main()
 {
     // write your code here
-
-    // std::vector songList = 
-
-
-
-    std::cout << "\nEnter the song's title: "; 
-
-    std::cout << "\nEnter artist name: ";
-
-    std::cout << "\nEneter duration of the song: "
-
+   
     std::vector<std::string> user_song = {};
+
+    std::string choice = "y";
+
+    while (choice != "n")
+    {
+        std::cout << "\nEnter the song's title: "; 
+        std::string name; 
+        std::cin >> name;
+
+        std::cout << "\nEnter the artist: ";
+        std::string artist;
+        std::cin >> artist;
+
+        std::cout << "\nDuration of song (in seconds): ";
+        std::string duration;
+        std::cin >> duration; 
+
+        std::cout << "\nWould you like to enter another song? (y/n)\n";
+        std::cin >> choice;
+    }
+   
     return 0;
 }
