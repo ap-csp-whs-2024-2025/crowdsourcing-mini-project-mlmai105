cdca9f472ef647efd5adb0c68e7274559d2ff831
mlmai105 <mollyle43@gmail.com>
2025-02-20 09:41:57 +0000

    :/

--- crowdsourcing.cpp
+++ crowdsourcing.cpp
@@ -11,18 +11,56 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
-// std::vector<std::string> (std::vector<std::string> user_song)
-// {
-//     while (count < size)
-//     {
-//         if 
-//         {
-//             append(user_song, )
-//         }
-//     }
+std::vector<std::string> (std::vector<std::string> name)
+{
+    
+    int curr_index = 0
+    while (count < size)
+    {
+        if 
+        {
+            append(user_song, )
+        }
+    }
 
-//     return user_song
-// }
+    return user_song
+}
+
+int TotalDuration (std::vector<int> duration)
+{
+    int total = 0;
+    int sum = 0;
+    int new_value = 0 
+    while (duration.size() != 1)
+    {
+        if (curr_index + 1 < duration.size())
+        {
+            new_value = duration[curr_index] + duration[curr_index + 1];
+            sum = new_value + duration[curr_index];
+            total = sum; 
+        }
+        
+    }
+    return total;
+
+}
+
+
+
+
+if (choice = y)
+{
+    count = count + 1;
+}
+
+   int times ()
+   {
+    if (choice == "y")
+        {
+            count = count + 1; 
+        }
+   }
+
 
 
 
@@ -34,10 +72,17 @@ int main()
    
     std::vector<std::string> user_song = {};
 
+
+    // times = ()
+    int count = 0
+    int length = 0
+
     std::string choice = "y";
 
-    while (choice != "n")
+    if (choice != "n")
     {
+    
+
         std::cout << "\nEnter the song's title: "; 
         std::string name; 
         std::cin >> name;
@@ -47,12 +92,12 @@ int main()
         std::cin >> artist;
 
         std::cout << "\nDuration of song (in seconds): ";
-        std::string duration;
+        std::int duration;
         std::cin >> duration; 
 
         std::cout << "\nWould you like to enter another song? (y/n)\n";
         std::cin >> choice;
+      
     }
-   
     return 0;
 }
